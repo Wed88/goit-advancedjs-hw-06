a3483e36641ecf43cd0c82c000e587eb3daae81d
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-12-02 01:08:04 +0200

    додає виконання 3 и 4 generic завдання

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
+class Component<T> {
+  constructor(public props: T) {
+    
   }
 }
+ 
+interface Element {
+  title: string;
+}
 
-class Page extends Component {
+class Page extends Component<Element> {
   pageInfo () {
     console.log(this.props.title);
   }
