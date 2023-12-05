b54dbc87902f6a9518bfff47dfc764ac49878599
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-12-05 22:26:33 +0200

    перероблює 6, 7 завдання

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,23 +3,15 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 enum Day {
-  SUNDAY,
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY,
+  SUNDAY = "weekend",
+  MONDAY = "weekday",
+  TUESDAY = "weekday",
+  WEDNESDAY = "weekday",
+  THURSDAY = "weekday",
+  FRIDAY = "weekday",
+  SATURDAY = "weekend",
 };
 
-const weekend = {
-  sunday: Day.SUNDAY,
-  saturday: Day.SATURDAY,
-}
-
-function isWeekend() {
-  if (Day.SUNDAY === weekend.sunday || Day.SATURDAY === weekend.saturday) {
-    return "yes"
-  }
-  return "no"
+function isWeekend(day: Day): boolean {
+  return day === "weekend"
 }
